454f5ad9fcfd8d829fbe5653f062348e689ef460
Jennifer Stoos <jstoos@bitlaw.com>
2017-11-28 00:34:21 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,7 @@
 
 def key_for_min_value(name_hash)
   smallest = 1000
-  name_hash.each do |thing, number|
+  name_hash.each do |thing=0, number=0|
     if number < smallest
         smallest = number && printme = thing
     end
